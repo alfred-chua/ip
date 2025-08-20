15038300dfb853a2b7938d03b432b39d0369302a
alfred-chua <e1406317@u.nus.edu>
2025-08-20 14:16:30 +0800

    Level 0 done

---
+++ src/main/java/Luke.java
@@ -0,0 +1,9 @@
+public class Luke {
+    public static void main(String[] args) {
+
+        String msg = "Hello! I'm Luke \n"
+                + "What can I do for you?\n\n"
+                + "Bye. Hope to see you again soon!";
+        System.out.println(msg);
+    }
+}
