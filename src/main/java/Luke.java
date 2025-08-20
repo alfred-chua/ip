bf5358fbb5e6d6377bd97673d03f55cde567eb11
alfred-chua <e1406317@u.nus.edu>
2025-08-20 14:47:57 +0800

    Level 1 done

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,9 +1,22 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Luke {
     public static void main(String[] args) {
 
-        String msg = "Hello! I'm Luke \n"
-                + "What can I do for you?\n\n"
-                + "Bye. Hope to see you again soon!";
-        System.out.println(msg);
+        Scanner sc = new Scanner(System.in);
+        String greeting = "Hello! I'm Luke \n"
+                + "What can I do for you?\n";
+        String goodbye = "Bye. Hope to see you again soon!";
+
+        System.out.println(greeting);
+        String input = "";
+
+         while (!Objects.equals(input, "bye")) {
+             System.out.println(input);
+             input = sc.nextLine();
+         }
+
+        System.out.println(goodbye);
     }
 }
