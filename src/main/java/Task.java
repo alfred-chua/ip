88099a734f67e009106c58419d7b776be09b87f7
alfred-chua <e1406317@u.nus.edu>
2025-08-20 21:35:13 +0800

    Add Task class and child classes

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+
+    protected String description;
+    protected boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public void complete() {
+        this.completed = true;
+    }
+
+    public String toString() {
+        if (this.completed) {
+            return "[X] " + this.description;
+        } else {
+            return "[ ] " + this.description;
+        }
+    }
+}
