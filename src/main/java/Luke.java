94027d6529e5f2d5292033680c57ef0f65304480
alfred-chua <e1406317@u.nus.edu>
2025-08-20 15:10:16 +0800

    Level 2 done

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -6,17 +6,26 @@ public class Luke {
 
         Scanner sc = new Scanner(System.in);
         String greeting = "Hello! I'm Luke \n"
-                + "What can I do for you?\n";
+                + "What can I do for you?";
         String goodbye = "Bye. Hope to see you again soon!";
+        String input = "";
+        String[] texts = new String[100];
+        int len = 0;
 
         System.out.println(greeting);
-        String input = "";
 
-         while (!Objects.equals(input, "bye")) {
-             System.out.println(input);
-             input = sc.nextLine();
-         }
-
+        while (!Objects.equals(input, "bye")) {
+            if (Objects.equals(input, "list")) {
+                for (int i = 1; i <= len; i++) {
+                    System.out.println(i + ". " + texts[i - 1]);
+                }
+            } else if (!Objects.equals(input, "")) {
+                 texts[len] = input;
+                 len++;
+                 System.out.println("added: " + input);
+            }
+            input = sc.nextLine();
+        }
         System.out.println(goodbye);
     }
 }
