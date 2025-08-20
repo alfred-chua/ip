6b056c66e3189f947acdaa8142a039247ef5cc81
alfred-chua <e1406317@u.nus.edu>
2025-08-20 21:47:31 +0800

    Replace array of strings to array of Tasks

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -9,11 +9,7 @@ public class Luke {
                 + "What can I do for you?";
         String goodbye = "Bye. Hope to see you again soon!";
         String input = "";
-        String[] texts = new String[100];
-        boolean[] done = new boolean[100];
-        for (int i = 0; i < 100; i++) {
-            done[i] = false;
-        }
+        Task[] tasks = new Task[100];
         int len = 0;
 
         System.out.println(greeting);
@@ -22,23 +18,19 @@ public class Luke {
 
             if (Objects.equals(input, "list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 1; i <= len; i++) {
-                    if (done[i - 1]) {
-                        System.out.println(i + ".[X] " + texts[i - 1]);
-                    } else {
-                        System.out.println(i + ".[ ] " + texts[i - 1]);
-                    }
+                for (int i = 0; i < len; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
                 }
 
             } else if (input.startsWith("mark ")) {
                 String strIndex = input.substring(5);
                 int index = Integer.parseInt(strIndex);
-                done[index - 1] = true;
+                tasks[index - 1].complete();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[X] " + texts[index - 1]);
+                System.out.println(tasks[index - 1]);
 
             } else if (!Objects.equals(input, "")) {
-                 texts[len] = input;
+                 tasks[len] = new Task(input);
                  len++;
                  System.out.println("added: " + input);
             }
