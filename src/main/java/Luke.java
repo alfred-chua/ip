b963d1e01df2a17481875902ecb3de9cb342dafc
alfred-chua <e1406317@u.nus.edu>
2025-08-22 12:53:24 +0800

    Add more exception handling

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -17,22 +17,51 @@ public class Luke {
         while (!Objects.equals(input, "bye")) {
 
             if (Objects.equals(input, "list")) {
-                System.out.println("Here are the tasks in your list:");
                 int len = tasks.size();
-                for (int i = 0; i < len; i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                if (len == 0) {
+                    System.out.println("You have no tasks in your list");
+                } else {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < len; i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
                 }
+
 
             } else if (input.startsWith("mark ")) {
                 String strIndex = input.substring(5);
-                int index = Integer.parseInt(strIndex);
+                int index;
+                try {
+                    index = Integer.parseInt(strIndex);
+                    if (index <= 0 || index > tasks.size()) {
+                        System.out.println("Error: marked value cannot be negative or more than list size");
+                        input = "";
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: marked value must be a number");
+                    input = "";
+                    continue;
+                }
                 tasks.get(index - 1).complete();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(tasks.get(index - 1));
 
             } else if (input.startsWith("delete ")) {
                 String strIndex = input.substring(7);
-                int index = Integer.parseInt(strIndex);
+                int index;
+                try {
+                    index = Integer.parseInt(strIndex);
+                    if (index <= 0 || index > tasks.size()) {
+                        System.out.println("Error: deleted value cannot be negative or more than list size");
+                        input = "";
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: deleted value must be a number");
+                    input = "";
+                    continue;
+                }
                 System.out.println("Noted. I've removed this task:");
                 System.out.println(tasks.get(index - 1));
                 tasks.remove(index - 1);
@@ -61,6 +90,12 @@ public class Luke {
                     }
                     description = input.substring(9, byIndex);
                     String by = input.substring(byIndex + 3);
+
+                    if (by.replaceAll("\\s", "").isEmpty()) {
+                        System.out.println("Error: by date cannot be empty");
+                        input = "";
+                        continue;
+                    }
                     task = new Deadline(description, by);
                 } else {
                     int fromIndex = input.indexOf("/from ");
@@ -73,6 +108,17 @@ public class Luke {
                     description = input.substring(6, fromIndex);
                     String from = input.substring(fromIndex + 5, toIndex);
                     String to = input.substring(toIndex + 3);
+
+                    if (from.replaceAll("\\s", "").isEmpty()) {
+                        System.out.println("Error: from date cannot be empty");
+                        input = "";
+                        continue;
+                    }
+                    if (to.replaceAll("\\s", "").isEmpty()) {
+                        System.out.println("Error: to date cannot be empty");
+                        input = "";
+                        continue;
+                    }
                     task = new Event(description, from, to);
                 }
 
